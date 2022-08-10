4adbde5f35dff2dd26d59d6716a1d3337282e712
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-10 16:29:13 -0300

    Home slide update

--- js/script.js
+++ js/script.js
@@ -20,4 +20,33 @@ window.onscroll = () => {
 }
 
 
+/* Home Slider */
+let slides = document.querySelectorAll('.home .slide');
+let = index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
