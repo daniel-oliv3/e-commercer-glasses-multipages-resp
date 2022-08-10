d6be3509cca7502f4bc9434e76304886e8e6452d
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-09 23:45:03 -0300

    Navbar toggle

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
+/* */
 let searchForm = document.querySelector('.header .search-form');
 
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
+}
+
+
+
+let navbar = document.querySelector('.header .navbar');
+
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
 }
