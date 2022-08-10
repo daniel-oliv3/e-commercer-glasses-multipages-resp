0e49b2d74eb88e0833719bad83890c1e7932b973
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-09 23:57:36 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
-/* */
+/* Search Toggle */
 let searchForm = document.querySelector('.header .search-form');
-
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
 }
 
-
-
+/* Navbar Toggle */
 let navbar = document.querySelector('.header .navbar');
-
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
 }
+
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+
